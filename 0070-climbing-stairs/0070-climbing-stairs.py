0c66f08f6ca36d99d217ad32cad2b54ceef13beb
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2024-10-01 13:50:13 +0300

    Time: 33 ms (66.69%), Space: 16.5 MB (64.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,7 +3,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n in Solution.hashtable:
             return Solution.hashtable[n]
-        if n == 1 or n == 0:
+        if n == 0 or n == 1:
             return 1
         
         Solution.hashtable[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
