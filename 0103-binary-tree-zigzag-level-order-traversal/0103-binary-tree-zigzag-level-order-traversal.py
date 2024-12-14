c178a057f0f3397bb0c67f49a99f5f0d7858d8fd
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2024-12-14 15:10:04 +0200

    Time: 0 ms (100%), Space: 17.7 MB (8.17%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -1,29 +1,30 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        result = []
+        stack = [root]
+        res = []
         if not root:
-            return result
-        queue = []
-        queue.append(root)
-        left = True
-        while queue:
-            levelarr = []
-            for i in range(len(queue)):
-                curr = queue.pop(0)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-                if left:
-                    levelarr.append(curr.val)
+            return res
+
+        leftToRight = True
+        while stack:
+            temp = []
+            length = len(stack)
+            for i in range(length):
+                curr = stack.pop(0)
+                if leftToRight:
+                    temp.append(curr.val)
                 else:
-                    levelarr.insert(0, curr.val)
-            left = not left
-            result.append(levelarr)
-        return result
+                    temp.insert(0, curr.val)
+                if curr.left:
+                    stack.append(curr.left)
+                if curr.right:
+                    stack.append(curr.right)
+            leftToRight = not leftToRight
+            res.append(temp)
+        return res
