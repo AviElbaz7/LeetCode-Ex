593c2700e8c1f1ac8ef2679b56d081e0e877b9a7
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-02-26 17:13:22 +0200

    Time: 816 ms (40.59%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-SELECT author_id AS id
+SELECT DISTINCT author_id AS id
 FROM Views
-GROUP BY author_id, viewer_id
-HAVING COUNT(*) >= 1 AND author_id = viewer_id
-ORDER BY author_id ASC
+WHERE author_id = viewer_id
+ORDER BY id
