239dcf116ba5d037cae0dc9718ad6c67dfef8e2a
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-01 20:49:08 +0200

    Time: 1035 ms (13.89%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    IFNULL(MAX(num), NULL) AS num
+FROM
+    MyNumbers
+WHERE num IN (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) <= 1
+)
