57b644f4d7c130f6735b8e98bab3f9ec219e5fe9
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-16 16:53:12 +0200

    Time: 233 ms (87.94%), Space: 20 MB (82.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         total = 0
-        def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != "1":
-                return
-            
-            temp = grid[i][j]
-            grid[i][j] = "#"
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-            return
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
+        rows, cols = len(grid), len(grid[0])
+        q = deque()
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
                     total += 1
-                    dfs(i, j)
+                    q.append((r, c))
+                    grid[r][c] = "0"
+                    while q:
+                        i, j = q.popleft()
+                        for dr, dc in directions:
+                            nr, nc = i + dr, j + dc
+                            if 0 <= nr <= rows - 1 and 0 <= nc <= cols - 1 and grid[nr][nc] == "1":
+                                grid[nr][nc] = "0"
+                                q.append((nr, nc))
         return total
