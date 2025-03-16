1566a89a1541a28f68fea780f0486254153e6e6b
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-16 17:07:18 +0200

    Time: 236 ms (82.89%), Space: 20.2 MB (69.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,20 +2,22 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         total = 0
         rows, cols = len(grid), len(grid[0])
-        q = deque()
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        def dfs(i, j):
+            if i < 0 or i > rows - 1 or j < 0 or j > cols - 1 or grid[i][j] != "1":
+                return
+            
+            grid[i][j] = "0"
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+            return
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
                     total += 1
-                    q.append((r, c))
-                    grid[r][c] = "0"
-                    while q:
-                        i, j = q.popleft()
-                        for dr, dc in directions:
-                            nr, nc = i + dr, j + dc
-                            if 0 <= nr <= rows - 1 and 0 <= nc <= cols - 1 and grid[nr][nc] == "1":
-                                grid[nr][nc] = "0"
-                                q.append((nr, nc))
+                    dfs(r, c)
         return total
+                    
