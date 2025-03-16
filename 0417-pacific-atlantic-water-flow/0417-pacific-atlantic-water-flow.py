077a1a4e4c3cdda132ed132ba122afe98992975d
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-16 21:16:21 +0200

    Time: 23 ms (94.53%), Space: 19.4 MB (64.43%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -18,13 +18,12 @@ class Solution:
             visited = set(starting_points)
             directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
             while q:
-                for i in range(len(q)):
-                    r, c = q.popleft()
-                    for dr, dc in directions:
-                        nr, nc = r + dr, c + dc
-                        if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c] and (nr, nc) not in visited:
-                            q.append([nr, nc])
-                            visited.add((nr, nc))
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c] and (nr, nc) not in visited:
+                        q.append([nr, nc])
+                        visited.add((nr, nc))
             return visited
         
         to_pacific = bfs(pacific)
