fb99e9a121a9facba0a1ea2268572ae3999884c1
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-19 11:20:42 +0200

    Time: 1934 ms (11.35%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,11 +1,10 @@
 # Write your MySQL query statement below
+WITH fisrt_orders AS (
+    SELECT *
+    FROM Delivery AS d1
+    WHERE order_date = (SELECT MIN(order_date) FROM Delivery AS d2 WHERE d1.customer_id = d2.customer_id)
+)
 SELECT
-    ROUND(IFNULL(100*AVG(order_date = customer_pref_delivery_date), 0), 2) AS immediate_percentage
+    ROUND(AVG(IF(order_date = customer_pref_delivery_date, 1, 0)) * 100, 2) AS immediate_percentage
 FROM
-    Delivery
-WHERE
-    (customer_id, order_date) IN (
-        SELECT customer_id, MIN(order_date)
-        FROM Delivery
-        GROUP BY customer_id
-    )
+    fisrt_orders
