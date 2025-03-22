6f9a6a7137cfc2907937fbf55560523caf881624
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-22 18:19:12 +0200

    Time: 1271 ms (93.65%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,39 +1,25 @@
 # Write your MySQL query statement below
 
-# number of movies watched by each user
+# name of user who watched most movies
 WITH num_movies_user_watched AS (
-    SELECT user_id, COUNT(DISTINCT movie_id) AS num_movies_watched
+    SELECT name
     FROM MovieRating AS mr
+    NATURAL JOIN Users AS u
     GROUP BY user_id
+    ORDER BY COUNT(DISTINCT movie_id) DESC, name
+    LIMIT 1
 ),
 # average rating of each movie for february 2020
 average_rating_per_movie AS (
-    SELECT movie_id, AVG(rating) AS avg_rating
+    SELECT title
     FROM MovieRating
+    NATURAL JOIN Movies
     WHERE YEAR(created_at) = 2020 AND MONTH(created_at) = 2
     GROUP BY movie_id
-),
-# answer part 1
-user_rated_most_movies AS (
-    SELECT u.name AS results
-    FROM num_movies_user_watched AS n
-    JOIN Users AS u
-    ON n.user_id = u.user_id
-    WHERE n.num_movies_watched = (SELECT MAX(num_movies_watched) FROM num_movies_user_watched)
-    ORDER BY name
-    LIMIT 1
-),
-# answer part 2
-top_average_rated_movie AS (
-    SELECT m1.title AS results
-    FROM average_rating_per_movie AS arpm
-    JOIN Movies AS m1
-    ON arpm.movie_id = m1.movie_id
-    WHERE arpm.avg_rating = (SELECT MAX(avg_rating) FROM average_rating_per_movie)
-    ORDER BY title
+    ORDER BY AVG(rating) DESC, title
     LIMIT 1
 )
 
-SELECT * FROM user_rated_most_movies
+SELECT name AS results FROM num_movies_user_watched
 UNION ALL
-SELECT * FROM top_average_rated_movie
+SELECT title FROM average_rating_per_movie
