e734866c4c8b2c38894f6de3864065b0242bc4ca
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-22 15:19:04 +0200

    Time: 2024 ms (83.43%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,21 +1,23 @@
 # Write your MySQL query statement below
+WITH categorized_bank_accounts AS (
+    SELECT
+    SUM(IF(income < 20000, 1, 0)) AS low_salary,
+    SUM(IF(income BETWEEN 20000 AND 50000, 1, 0)) AS average_salary,
+    SUM(IF(income > 50000, 1, 0)) AS high_salary
+    FROM
+    Accounts
+)
 SELECT
-    "Low Salary" AS category, COUNT(account_id) AS accounts_count
+    'Low Salary' AS category, low_salary AS accounts_count
 FROM
-    Accounts AS a
-WHERE
-    income < 20000
-UNION
+    categorized_bank_accounts
+UNION ALL
 SELECT
-    "Average Salary" AS category, COUNT(account_id) AS accounts_count
+    'Average Salary' AS category, average_salary AS accounts_count
 FROM
-    Accounts AS a
-WHERE
-    income BETWEEN 20000 AND 50000
-UNION
+    categorized_bank_accounts
+UNION ALL
 SELECT
-    "High Salary" AS category, COUNT(account_id) AS accounts_count
+    'High Salary' AS category, high_salary AS accounts_count
 FROM
-    Accounts AS a
-WHERE
-    income > 50000
+    categorized_bank_accounts
