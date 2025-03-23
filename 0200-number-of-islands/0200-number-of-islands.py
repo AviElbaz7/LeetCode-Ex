8cf6e24ca0ca4b2bb20136f177e82702076a9891
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-23 09:46:05 +0200

    Time: 246 ms (63.13%), Space: 19.9 MB (99.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         total = 0
-        rows, cols = len(grid), len(grid[0])
-
-        def dfs(i, j):
-            if i < 0 or i > rows - 1 or j < 0 or j > cols - 1 or grid[i][j] != "1":
-                return
-            
-            grid[i][j] = "0"
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-            return
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1":
+        n, m = len(grid), len(grid[0])
+        q = deque()
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
                     total += 1
-                    dfs(r, c)
+                    q.append([i, j])
+                    grid[i][j] = "0"
+                    while q:
+                        xx, yy = q.popleft()
+                        for x, y in directions:
+                            nx, ny = xx + x, yy + y
+                            if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == "1":
+                                grid[nx][ny] = "0"
+                                q.append([nx, ny])
         return total
-                    
+
