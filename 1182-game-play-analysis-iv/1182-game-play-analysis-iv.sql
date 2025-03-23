d2be25ee63dcc4b11428da2f4f79da5025486c58
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-23 23:50:38 +0200

    Time: 1369 ms (23.42%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,15 +1,11 @@
 # Write your MySQL query statement below
-# first login
 WITH first_login AS (
-    SELECT player_id, MIN(event_date) AS first_date
-    FROM Activity AS a1
+    SELECT player_id, MIN(event_date) AS min_date
+    FROM Activity
     GROUP BY player_id
 )
-SELECT
-    ROUND(COUNT(DISTINCT f.player_id) / COUNT(DISTINCT a.player_id), 2) AS fraction
-FROM
-    Activity AS a
-LEFT JOIN
-    first_login AS f
-ON
-    a.player_id = f.player_id AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY)
+
+SELECT ROUND(COUNT(DISTINCT a.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM Activity AS a
+JOIN first_login AS f
+ON a.player_id = f.player_id AND a.event_date = DATE_ADD(f.min_date, INTERVAL 1 DAY)
