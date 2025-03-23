5c93bc8879d0f64820f12916649057c243848d93
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-23 13:45:42 +0200

    Time: 28 ms (79.3%), Space: 19.3 MB (80.56%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,32 +1,32 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not heights:
-            return []
-
         pacific, atlantic = [], []
+        q = deque()
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         rows, cols = len(heights), len(heights[0])
 
         for r in range(rows):
             pacific.append((r, 0))
             atlantic.append((r, cols - 1))
+            
         for c in range(cols):
             pacific.append((0, c))
             atlantic.append((rows - 1, c))
         
-        def bfs(starting_points):
-            q = deque(starting_points)
-            visited = set(starting_points)
-            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        def bfs(starts):
+            visited = set(starts)
+            q = deque(starts)
             while q:
-                r, c = q.popleft()
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c] and (nr, nc) not in visited:
-                        q.append([nr, nc])
-                        visited.add((nr, nc))
+                for i in range(len(q)):
+                    x, y = q.popleft()
+                    for dx, dy in directions:
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < rows and 0 <= ny < cols and heights[nx][ny] >= heights[x][y] and (nx, ny) not in visited:
+                            q.append((nx, ny))
+                            visited.add((nx, ny))
             return visited
-        
-        to_pacific = bfs(pacific)
-        to_atlantic = bfs(atlantic)
+            
+        pacific_reach = bfs(pacific)
+        atlantic_reach = bfs(atlantic)
 
-        return list(to_pacific & to_atlantic)
+        return list(pacific_reach & atlantic_reach)
