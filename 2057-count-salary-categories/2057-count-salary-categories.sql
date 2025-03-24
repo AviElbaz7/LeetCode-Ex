782bafdff727825a748de20dc0b71c4bdd6ccab9
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-24 19:38:08 +0200

    Time: 1811 ms (88.99%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,23 +1,25 @@
 # Write your MySQL query statement below
-WITH categorized_bank_accounts AS (
-    SELECT
-    SUM(IF(income < 20000, 1, 0)) AS low_salary,
-    SUM(IF(income BETWEEN 20000 AND 50000, 1, 0)) AS average_salary,
-    SUM(IF(income > 50000, 1, 0)) AS high_salary
-    FROM
-    Accounts
+WITH high_accounts AS (
+    SELECT *
+    FROM Accounts
+    WHERE income > 50000
+),
+average_accounts AS (
+    SELECT *
+    FROM Accounts
+    WHERE income BETWEEN 20000 AND 50000
+),
+low_accounts AS (
+    SELECT *
+    FROM Accounts
+    WHERE income < 20000
 )
-SELECT
-    'Low Salary' AS category, low_salary AS accounts_count
-FROM
-    categorized_bank_accounts
+
+SELECT "High Salary" AS category, IFNULL(COUNT(*), 0) AS accounts_count
+FROM high_accounts
 UNION ALL
-SELECT
-    'Average Salary' AS category, average_salary AS accounts_count
-FROM
-    categorized_bank_accounts
+SELECT "Low Salary" AS category, IFNULL(COUNT(*), 0) AS accounts_count
+FROM low_accounts
 UNION ALL
-SELECT
-    'High Salary' AS category, high_salary AS accounts_count
-FROM
-    categorized_bank_accounts
+SELECT "Average Salary" AS category, IFNULL(COUNT(*), 0) AS accounts_count
+FROM average_accounts
