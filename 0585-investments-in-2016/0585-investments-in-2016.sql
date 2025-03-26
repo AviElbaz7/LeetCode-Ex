b35b6c61ee41dbec5cabddfc7d793da9bc9827a1
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-26 21:55:20 +0200

    Time: 599 ms (90.21%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM Insurance
-WHERE pid IN (
-    SELECT i1.pid
-    FROM Insurance AS i1
-    JOIN Insurance AS i2
-    ON i1.pid <> i2.pid AND i1.tiv_2015 = i2.tiv_2015
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(pid) > 1
 )
 AND (lat, lon) IN (
     SELECT lat, lon
