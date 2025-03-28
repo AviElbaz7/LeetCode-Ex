9bacbe82758567fff577dfcde8501600d1c9cbe3
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-28 21:44:14 +0300

    Time: 443 ms (98.45%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-WITH until_2019_08_16 AS (
+WITH last_date_per_product AS (
     SELECT product_id, MAX(change_date) AS last_date
     FROM Products
     WHERE change_date <= '2019-08-16'
@@ -8,11 +8,8 @@ WITH until_2019_08_16 AS (
 
 SELECT DISTINCT product_id, 10 AS price
 FROM Products
-WHERE product_id NOT IN (SELECT product_id FROM until_2019_08_16)
-
+WHERE product_id NOT IN (SELECT product_id FROM last_date_per_product)
 UNION ALL
-
-SELECT u.product_id, new_price
-FROM until_2019_08_16 AS u
-JOIN Products AS p
-ON u.product_id = p.product_id AND u.last_date = p.change_date
+SELECT DISTINCT product_id, new_price
+FROM Products
+WHERE (product_id, change_date) IN (SELECT product_id, last_date FROM last_date_per_product)
