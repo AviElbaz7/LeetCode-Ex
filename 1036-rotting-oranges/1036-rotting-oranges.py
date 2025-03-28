fc8044d64c17943b06b7536892311b831de9be3d
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-28 22:49:10 +0300

    Time: 7 ms (27.43%), Space: 17.7 MB (93.37%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -0,0 +1,33 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        q = deque()
+        directions = [(1, 0), (-1, 0), (0,-1), (0, 1)]
+        min_minutes = 0
+        first_minute = True
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append([r, c])
+        
+        while q:
+            n = len(q)
+            for i in range(n):
+                x, y = q.popleft()
+                for dx, dy in directions:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == 1:
+                        if first_minute:
+                            min_minutes += 1
+                            first_minute = False
+                        q.append([nx, ny])
+                        grid[nx][ny] = 2
+            first_minute = True
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    return -1
+        
+        return min_minutes
