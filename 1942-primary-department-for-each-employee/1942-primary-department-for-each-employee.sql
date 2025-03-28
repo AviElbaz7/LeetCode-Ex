4bf152709b5c26a7075759faaf2cdb09391d4b6e
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-28 18:20:36 +0300

    Time: 552 ms (93.91%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,18 +1,9 @@
 # Write your MySQL query statement below
-WITH employees_with_one_department AS (
-    SELECT employee_id, MIN(department_id) AS department_id
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y' OR employee_id IN (
+    SELECT employee_id
     FROM Employee
     GROUP BY employee_id
     HAVING COUNT(department_id) = 1
-),
-employees_with_more_departments AS (
-    SELECT employee_id, department_id
-    FROM Employee
-    WHERE primary_flag = 'Y'
 )
-
-SELECT *
-FROM employees_with_one_department
-UNION ALL
-SELECT *
-FROM employees_with_more_departments
