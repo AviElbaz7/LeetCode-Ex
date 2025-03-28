910a5173b3dda89764eee1642292894ea08d4e34
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-29 01:17:19 +0300

    Time: 742 ms (98.57%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,9 +1,11 @@
 # Write your MySQL query statement below
-WITH comulative_weights_by_turn AS (
-    SELECT person_name, SUM(weight) OVER (ORDER BY turn) AS comulative
+WITH comulative_wheights AS (
+    SELECT person_id, person_name, turn, SUM(weight) OVER (ORDER BY turn) AS num_weight
     FROM Queue
 )
 
 SELECT person_name
-FROM comulative_weights_by_turn
-WHERE comulative = (SELECT MAX(comulative) AS new FROM comulative_weights_by_turn WHERE comulative <= 1000)
+FROM comulative_wheights
+WHERE num_weight <= 1000
+ORDER BY num_weight DESC
+LIMIT 1
