fadee41ecc1b35db2d0c1522a08156213faa12cb
Avi Elbaz <143399786+AviElbaz7@users.noreply.github.com>
2025-03-28 18:17:33 +0300

    Time: 520 ms (97.83%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,16 +1,18 @@
 # Write your MySQL query statement below
-SELECT
-    employee_id, department_id
-FROM
-    Employee AS e1
-WHERE
-    primary_flag = 'Y'
-UNION
-SELECT
-    employee_id, department_id
-FROM
-    Employee AS e2
-GROUP BY
-    employee_id
-HAVING
-    COUNT(department_id) = 1
+WITH employees_with_one_department AS (
+    SELECT employee_id, MIN(department_id) AS department_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(department_id) = 1
+),
+employees_with_more_departments AS (
+    SELECT employee_id, department_id
+    FROM Employee
+    WHERE primary_flag = 'Y'
+)
+
+SELECT *
+FROM employees_with_one_department
+UNION ALL
+SELECT *
+FROM employees_with_more_departments
